9589b7ae97113c7b2c811d19f1481587a0612f30
Duncan Campbell <duncan.alan.campbell@gmail.com>
2014-07-08 17:40:19 -0600

    added test for inside.py and added example amin function to inside.py

---
+++ tests/test_inside.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from ..inside import inside
+import numpy as np
+
+def test_match_one_to_one():
+    #define square
+    square = [(-1,-1),(-1,1),(1,1),(1,-1)]
+    
+    #define test points
+    p1 = (0,0) #inside
+    p2 = (-1,2) #outside
+    
+    result = inside(p1[0],p1[1],square)
+    
+    assert result, "failed to locate point inside polygon"
+    
+    result = inside(p2[0],p2[1],square)
+    
+    assert not result, "failed to locate point outside polygon"
