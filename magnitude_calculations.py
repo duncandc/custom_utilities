df73e8c466be1b96b1207bc89cfe8a1e4132e56c
Duncan Campbell <duncan.alan.campbell@gmail.com>
2014-09-18 18:43:07 +0300

    fixed some doc strings. added a function to calc abs mag lim of flux limited survey

--- magnitude_calculations.py
+++ magnitude_calculations.py
@@ -8,9 +8,10 @@ Functions that compute quantities dealing with magnitudes.
 
 from __future__ import division
 
-__all__=['apparent_to_absolute_magnitude','luminosity_to_absolute_magnitude','get_sun_mag']
+__all__=['apparent_to_absolute_magnitude','luminosity_to_absolute_magnitude','absolute_magnitude_to_luminosity','get_sun_mag']
 
 import numpy as np
+
 
 def apparent_to_absolute_magnitude(m, d_L):
     """
@@ -41,7 +42,7 @@ def luminosity_to_absolute_magnitude(L, band, system='SDSS_Blanton_2003_z0.1'):
     Parameters
     ----------
     L: array_like
-        apparent magnitude
+        luminosity
     
     band: string
        filter band
@@ -62,6 +63,66 @@ def luminosity_to_absolute_magnitude(L, band, system='SDSS_Blanton_2003_z0.1'):
             
     return M
 
+
+def absolute_magnitude_to_luminosity(M, band, system='SDSS_Blanton_2003_z0.1'):
+    """
+    calculate the Luminosity
+    
+    Parameters
+    ----------
+    M: array_like
+        absolute magnitude
+    
+    band: string
+       filter band
+    
+    system: string, optional
+        filter systems: default is 'SDSS_Blanton_2003_z0.1'
+          1. Binney_and_Merrifield_1998
+          2. SDSS_Blanton_2003_z0.1
+    
+    Returns
+    -------
+    L: np.array of Luminosities in $log(L_{\odot})$
+    """
+    
+    Msun = get_sun_mag(band,system)
+    L = (M-Msun)/(-2.5) #in log(L/Lsun)
+            
+    return L
+
+
+def absolute_magnitude_lim(z, app_mag_lim, cosmo=None):
+    """
+    give the absolute magnitude limit as a function of redshift for a flux-limited survey.
+    
+    Parameters
+    ----------
+    M: array_like
+        absolute magnitude
+    
+    band: string
+       filter band
+    
+    system: string, optional
+        filter systems: default is 'SDSS_Blanton_2003_z0.1'
+          1. Binney_and_Merrifield_1998
+          2. SDSS_Blanton_2003_z0.1
+    
+    Returns
+    -------
+    M,z: np.array, np.array
+        absolute magnitude, redshift
+    """
+    if cosmo==None:
+        from astropy.cosmology import FlatLambdaCDM
+        cosmo = FlatLambdaCDM(H0=70, Om0=0.3)
+        print('Warning, no cosmology specified, using default:',cosmo)
+    
+    d_L = cosmo.luminosity_distance(z)
+    M = apparent_to_absolute_magnitude(app_mag_lim, d_L)
+    
+    return M
 
 def get_sun_mag(filter,system):
     """
