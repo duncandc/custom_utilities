c7ef21bff6ccda78a159ab1421783bbe3a923be9
Duncan Campbell <duncan.alan.campbell@gmail.com>
2015-07-31 11:57:31 -0400

    added import

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 __all__ = ["match","inside","spherematch","sample_ra_dec_box","sample_spherical_cap",\
            "sample_spherical_surface","get_path","histogram2d","f_prop",\
-           "magnitude_calculations","binned_std","schechter_function","fitting","statistics"]
+           "magnitude_calculations","binned_std","schechter_function","fitting","statistics","cosmology"]
 
 from match import match
 from inside import inside
@@ -11,6 +11,7 @@ from get_path import get_data_path
 from get_path import get_output_path
 from get_path import get_plot_path
 from get_path import get_base_path
+from get_path import get_system
 from histogram2d import histogram2d
 from spheredist import spheredist
 from spherematch import spherematch
@@ -26,3 +27,4 @@ import schechter_function
 import fitting
 import plotting
 import statistics
+import cosmology
